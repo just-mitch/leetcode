37648c7778b22c2aee8b9f7e1e965148c187081c
Mitchell Tracy <mitch@twnsqr.com>
2023-12-14 15:33:15 -0500

    Add data_structures module and swap_nodes_in_pairs module

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+mod data_structures;
 mod two_sum;
 mod merge_two_sorted_lists;
 mod generate_parentheses;
+mod swap_nodes_in_pairs;
--- src/merge_two_sorted_lists/mod.rs
+++ src/merge_two_sorted_lists/mod.rs
@@ -1,38 +1,5 @@
 // Definition for singly-linked list.
-#[derive(PartialEq, Eq, Clone, Debug)]
-pub struct ListNode {
-  pub val: i32,
-  pub next: Option<Box<ListNode>>
-}
-
-impl ListNode {
-  #[inline]
-  fn new(val: i32) -> Self {
-    ListNode {
-      next: None,
-      val
-    }
-  }
-
-  fn append(&mut self, val: i32) {
-    let mut current = self;
-    while current.next.is_some() {
-      current = current.next.as_mut().unwrap()
-    }
-    current.next = Some(Box::new(ListNode::new(val)));
-  }
-
-  fn from_vec(vec: Vec<i32>) -> Option<Box<ListNode>> {
-    let mut head = Some(Box::new(ListNode::new(0)));
-    let mut current = &mut head;
-    for val in vec {
-      current.as_mut().unwrap().append(val);
-      current = &mut current.as_mut().unwrap().next;
-    }
-    head.unwrap().next
-  }
-
-}
+use crate::data_structures::list_node::ListNode;
 
 pub struct Solution {}
 
